3dbe6956ec07e33f699a17c37c0c90b723c239ac
Saleha Shujaat <saleha.shujaat@bazaar-app.com>
2022-02-12 21:58:20 +0500

    JS variables defined

---
+++ src/variables/variables-1.js
@@ -0,0 +1,18 @@
+studentName = "Saleha Shujaat";
+basket = ['apple', 'banana', 1]
+student = {
+    name: 'Saleha',
+    age: '24',
+    profession: 'coding'
+}
+var myUndefined;
+var noInitialValue = null;
+
+console.log("studentName string", studentName);
+console.log("basket array", basket);
+console.log("student object", student);
+console.log("undefined", myUndefined);
+console.log("null type", noInitialValue);
+
+// scoping
+personName = "Saleha Shujaat"; // binds to the global scope which is the window object
